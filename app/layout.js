ed1aa82542117edccefc3ef152728445bb46f830
keyready <vip.korchak@list.ru>
2023-06-22 01:38:46 +0300

    добавил device-detect, чтобы не давать пользоваться с мобилок, есть какие-то ошибка, но почему меня должно это хоть как-то задевать?

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,8 @@
+'use client';
+
 import './globals.css';
 import { Inter } from 'next/font/google';
+import { MobileView, BrowserView } from 'react-device-detect';
 import { Header } from '@/components/Header';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -13,8 +16,16 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body className={inter.className}>
-                <Header />
-                <div className="page">{children}</div>
+                <MobileView className="notAllowed">
+                    <h2 style={{ width: '100%', textAlign: 'center' }}>
+                        Этот проект недоступен с мобильных устройств
+                    </h2>
+                    <p style={{ width: '100%', textAlign: 'center' }}>Перезайдите с компьютера</p>
+                </MobileView>
+                <BrowserView>
+                    <Header />
+                    <div className="page">{children}</div>
+                </BrowserView>
             </body>
         </html>
     );

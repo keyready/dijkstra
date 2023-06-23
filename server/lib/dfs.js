bfdffeebf6e084ca5f67f465f606db1104c3d334
keyready <93424230+keyready@users.noreply.github.com>
2023-06-24 00:11:08 +0300

    добавил переменное количество трок/столбцов в исходном графе, начал делать dfs

---
+++ server/lib/dfs.js
@@ -0,0 +1,38 @@
+/**
+ * Основная идея алгоритма:
+ * Создаем объект visited, который будет отслеживать, какие вершины мы уже посетили. Изначально все вершины помечены как непосещенные.
+ * Создаем пустой массив traversalOrder, который будет хранить порядок посещения вершин.
+ * В функции traverse мы передаем текущую вершину. Вначале помечаем ее как посещенную и добавляем в массив traversalOrder.
+ * Получаем список смежных вершин для текущей вершины из graph.
+ * Рекурсивно вызываем функцию traverse для каждой смежной вершины, если она не была посещена ранее.
+ * По завершении обхода возвращаем массив traversalOrder, который содержит вершины в порядке их посещения.
+ *
+ * @param graph - исходный граф
+ * @param startVertex - начальная вершина
+ * @returns {*[]} - порядок обхода вершин
+ */
+function dfs(graph, startVertex) {
+    const visited = {};
+    const traversalOrder = [];
+
+    function traverse(vertex) {
+        visited[vertex] = true;
+        traversalOrder.push(vertex);
+
+        const neighbors = graph[vertex];
+        for (let i = 0; i < neighbors.length; i += 1) {
+            const neighbor = neighbors[i];
+            if (!visited[neighbor]) {
+                traverse(neighbor);
+            }
+        }
+    }
+
+    traverse(startVertex);
+
+    return traversalOrder;
+}
+
+module.exports = {
+    dfs,
+};

9562cbbcac40a50635aee88a14740326cda3ec96
keyready <vip.korchak@list.ru>
2023-06-26 09:28:20 +0300

    пофиксил ошибку с кнопкой, изменил линки на главной

--- app/page.js
+++ app/page.js
@@ -33,36 +33,84 @@ export default function Home() {
             <h2 className={classes.title}>Содержание</h2>
             <h3>В работе реализованы следующие алгоритмы:</h3>
             <div className={classes.links}>
-                <Link href="/bfs">Обход графа в ширину</Link>
-                <Link href="/bfs">Обход графа в глубину</Link>
-                <Link href="/dijkstra">Алгоритм Дейкстры</Link>
-                <Link href="/ford-fulkerson">Алгоритм Форда-Фалкерсона</Link>
-                <Link href="/floyd">Алгоритм Флойда</Link>
+                <Link className={classes.referenceLinks} href="/bfs">
+                    Обход графа в ширину
+                </Link>
+                <Link className={classes.referenceLinks} href="/bfs">
+                    Обход графа в глубину
+                </Link>
+                <Link className={classes.referenceLinks} href="/dijkstra">
+                    Алгоритм Дейкстры
+                </Link>
+                <Link className={classes.referenceLinks} href="/ford-fulkerson">
+                    Алгоритм Форда-Фалкерсона
+                </Link>
+                <Link className={classes.referenceLinks} href="/floyd">
+                    Алгоритм Флойда
+                </Link>
             </div>
 
             <h2 className={classes.title}>Технологии</h2>
             <h3>Для выполнения работы были использованы следующие технологии:</h3>
             <ul className={classes.technologiesList}>
                 <li>
-                    {'Для визуального отображения создан веб-интерфейс на фреймфорке '}
-                    <a href="https://nextjs.org" target="_blank" rel="noreferrer">
+                    <span>{'Для визуального отображения создан веб-интерфейс на фреймфорке '}</span>
+                    <a
+                        className={classes.referenceLinks}
+                        href="https://nextjs.org"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
                         NextJs
+                    </a>
+                    <span>{' и '}</span>
+                    <a
+                        className={classes.referenceLinks}
+                        href="https://react.dev/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        ReactJs
                     </a>
                 </li>
                 <li>
-                    {'Для вычилений был написан сервер на '}
-                    <a href="https://nodejs.org" target="_blank" rel="noreferrer">
+                    <span>{'Для вычилений был написан сервер на '}</span>
+                    <a
+                        className={classes.referenceLinks}
+                        href="https://nodejs.org"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
                         NodeJs
                     </a>
-                    {' с использованием фреймворка '}
-                    <a href="https://expressjs.com" target="_blank" rel="noreferrer">
+                    <span>{' с использованием фреймворка '}</span>
+                    <a
+                        className={classes.referenceLinks}
+                        href="https://expressjs.com"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
                         ExpressJs
                     </a>
                 </li>
                 <li>
-                    {'Для хостинга был использован сервис '}
-                    <a href="https://vercel.com" target="_blank" rel="noreferrer">
+                    <span>{'Для хостинга был использован сервис '}</span>
+                    <a
+                        className={classes.referenceLinks}
+                        href="https://vercel.com"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
                         Vercel.com
+                    </a>
+                    <span>{' и '}</span>
+                    <a
+                        className={classes.referenceLinks}
+                        href="https://netlify.app"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        Netlify.app
                     </a>
                 </li>
             </ul>
--- components/Button/ui/Button.jsx
+++ components/Button/ui/Button.jsx
@@ -2,6 +2,7 @@ import classes from './Button.module.scss';
 import { classNames } from '@/lib/classNames';
 
 export function Button(props) {
+    // eslint-disable-next-line object-curly-newline
     const { children, className, type = 'primary', ...otherProps } = props;
 
     return (
